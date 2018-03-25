722b9a471a64b7f9f540322d7a169d1649d92316
Prudence Mok <mklprudence@gmail.com>
2018-03-26 01:51:06 +0800

    add token and prefix config to code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // setup global variables
-var prefix = 'm!';
+const config = require('./config.json');
+var prefix_default = config.prefix_default;
 var devlist = ['mklprud3nce', 'MineBerserker'];
+var prefix_MinerClan = config.prefix_MinerClan;
+var prefix_PR = config.prefix_PR;
 
 // require the discord.js module
 const Discord = require('discord.js');
@@ -17,22 +20,21 @@ client.on('ready', () => {
 });
 
 // login to Discord with your app's token
-client.login('NDI3MDQxNzkxNDA1OTgxNjk3.DZlqew.czX8VqGLNEoyzj2-TwdWT_PsjVc');
+client.login(config.token);
 
 client.on('message', message => {
-    // console.log(message.content);
-    if (message.content.substring(0, prefix.length) == prefix) {
-        var args = message.content.substring(prefix.length).split(' ');
-        var cmd = args[0];
-        var isdev = devlist.includes(message.author.username);
 
-        // cut out args[0] the cmd
-        args = args.splice(1);
+    switch(message.guild.id) {
 
-        switch(message.guild.id) {
+        // Miner's Clan
+        case '417584740758061056':
+            if (message.content.substring(0, prefix_MinerClan.length) == prefix_MinerClan) {
+                let args = message.content.substring(prefix_MinerClan.length).split(' ');
+                let cmd = args[0];
+                let isdev = devlist.includes(message.author.username);
 
-            // Miner's Clan
-            case '417584740758061056':
+                // cut out args[0] the cmd
+                args = args.splice(1);
                 // sense cmd
                 switch(cmd) {
                     case 'ping':
@@ -44,18 +46,28 @@ client.on('message', message => {
                         }
                         break;
                 }
-                break;
+            }
+            break;
 
-            // P&R dev Server
-            case '275235526330810369':
+        // P&R dev Server
+        case '275235526330810369':
+            if (message.content.substring(0, prefix_PR.length) == prefix_PR) {
+                let args = message.content.substring(prefix_PR.length).split(' ');
+                let cmd = args[0];
+                let isdev = devlist.includes(message.author.username);
+
+                // cut out args[0] the cmd
+                args = args.splice(1);
+
                 // sense cmd
                 switch(cmd) {
                     case 'ping':
                         message.channel.send('Dev Server ping pong madness!!!');
                         break;
                 }
-                break;
+            }
+            break;
 
-        }
     }
+
 });
