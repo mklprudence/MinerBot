6c9e0e8eecfb8d696c067eabaa66428ee108cbd2
Prudence Mok <mklprudence@gmail.com>
2018-04-04 12:05:02 +0800

    DB var => const

--- index.js
+++ index.js
@@ -326,7 +326,7 @@ client.on('message', async message => {
                 }
             }
             else if (cmd == 'help'){
-                const 
+                
             }
         }
     }
