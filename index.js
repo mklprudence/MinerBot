bd1229717198fa6b4169e011aaf421b66019bc72
Prudence Mok <mklprudence@gmail.com>
2018-04-07 08:41:53 +0800

    LoginCommand:Filter bot in all login commands

--- index.js
+++ index.js
@@ -151,6 +151,7 @@ client.on('message', async message => {
                             userSession.sort((a, b) => b.total_login - a.total_login)
                                 .filter(user => client.users.has(user.user_id))
                                 .filter(user => message.guild.members.has(user.user_id))
+                                .filter(user => !client.users.get(user.user_id).bot)
                                 .map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${Math.floor(user.total_login / (1000 * 60 * 60 * 24))} days ${Math.floor((user.total_login % 86400000) / (1000 * 60 * 60))} hours ${Math.floor((user.total_login % 3600000) / (1000 * 60))} Minutes ${Math.floor((user.total_login % 60000) / (1000))} Seconds`)
                                 .join('\n'),
                             { code: true, split: true }
@@ -354,6 +355,7 @@ client.on('message', async message => {
                             userSession.sort((a, b) => b.total_login - a.total_login)
                                 .filter(user => client.users.has(user.user_id))
                                 .filter(user => message.guild.members.has(user.user_id))
+                                .filter(user => !client.users.get(user.user_id).bot)
                                 .map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${Math.floor(user.total_login / (1000 * 60 * 60 * 24))} days ${Math.floor((user.total_login % 86400000) / (1000 * 60 * 60))} hours ${Math.floor((user.total_login % 3600000) / (1000 * 60))} Minutes ${Math.floor((user.total_login % 60000) / (1000))} Seconds`)
                                 .join('\n'),
                             { code: true, split: true }
