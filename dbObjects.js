5b357aa152d141643f1f0deb843fd8cef1d4b646
Prudence Mok <mklprudence@gmail.com>
2018-04-17 23:21:53 +0800

    change to heroku db

--- dbObjects.js
+++ dbObjects.js
@@ -1,10 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('epiz_21949064_MKLBot', 'epiz_21949064', 'mklprudence', {
+/* const sequelize = new Sequelize('epiz_21949064_MKLBot', 'epiz_21949064', 'mklprudence', {
     host: 'sql210.epizy.com',
     dialect: 'mysql',
     logging: false,
-});
+});*/
+
+const sequelize = new Sequelize('postgres://iabqwsabhctrbv:0ab16a079b652c0a05beccdf090ae4cae3a5c8c29448044c894117cd113e8f26@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d79ntsduij03ps');
 
 const Users = sequelize.import('models/Users');
 
