8227d56fabda2a5bf0892ba668d1cb5f5d7c6899
Prudence Mok <mklprudence@gmail.com>
2018-04-22 14:18:40 +0800

    console log msg content

--- index.js
+++ index.js
@@ -321,7 +321,7 @@ client.on('message', async message => {
                         .then(msg => msg.reactions.get(args[1]).users.forEach((user, key, map) => message.channel.send(user.username)))
                         .catch(console.error);*/
                     message.channel.fetchMessage(args[0])
-                        .then(msg => message.channel.send(msg.reactions.get(args[1]).users || 'no user here'))
+                        .then(msg => console.log(msg.reactions.get(args[1]).users || 'no user here'))
                         .catch(console.error);
                 }
             }
