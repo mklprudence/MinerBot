41652fd760047797f3029720786d5bab9199fbc4
Prudence Mok <mklprudence@gmail.com>
2018-05-07 01:17:23 +0800

    put RSS() into a function

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ client.once('ready', async () => {
     const storedUserSession = await Users.findAll();
     storedUserSession.forEach(s => userSession.set(s.user_id, s));
     console.log(`Logged in as ${client.user.tag}!`);
-    client.setInterval(RSS(), 30000);
+    client.setInterval(function() {RSS();}, 30000);
 });
 
 // login to Discord with your app's token
